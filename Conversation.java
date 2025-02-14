a7a8c10beac6b9919d1b1ce6ddd97f78b010db75
esommerfeld2 <esommerfeld@smith.edu>
2025-02-14 13:13:48 -0500

    submit

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,56 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
 
 class Conversation implements Chatbot {
 
   // Attributes 
+  Scanner in;
+  ArrayList<String> transcript;
 
   /**
    * Constructor 
    */
+
   Conversation() {
-    
+    this.in = new Scanner(System.in); /* Creates scanner called in */
+    this.transcript = new ArrayList<>(); /* Creates array called transcript */
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
+    /* Get's the number of rounds */
+    System.out.println("How many rounds?");
+    int rounds = in.nextInt();
+    in.nextLine();
+    System.out.println("What would you like to talk about?");
 
+    /* Loop that runs the converstaion for the correct number of rounds*/
+    for (int i= 0; i < rounds; i++) {
+      String inString = in.nextLine();
+      transcript.add(inString);
+      String resultOfChat = respond(inString);
+      transcript.add(resultOfChat);
+      System.out.println(resultOfChat);
+    }
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
+    /* Closes out the converstaion */
+    System.out.println("Thanks for chatting!");
+    System.out.println("TRANSCRIPT:");
+    System.out.println("What would you like to talk about?");
 
+    /* Creates loop that prints the converstaion*/
+    for(String s : transcript){
+      System.out.println(s);
+    }
+    System.out.println("Thanks for chatting!");
   }
 
   /**
@@ -30,8 +59,55 @@ class Conversation implements Chatbot {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+    /* Splits up the string into differnet words */
+    String[] words = inputString.split(" ");
+    String[] returnString = new String[words.length];
+    int count = 0; 
+    String theResponse;
+    /* Creates a loop that looks at each word and sees if it matches a key word*/
+    for (int i = 0; i <words.length; i++) {
+      if (words[i].equalsIgnoreCase("I")){;
+        returnString[i] = "you";
+      }
+      else if(words[i].equalsIgnoreCase("me")){;
+        returnString[i] = "you";
+      }
+      else if(words[i].equalsIgnoreCase("am")){;
+        returnString[i] = "are";
+      }
+      else if(words[i].equalsIgnoreCase("you")){;
+        returnString[i] = "I";
+      } 
+      else if(words[i].equalsIgnoreCase("my")){;
+        returnString[i] = "your";
+      }
+      else if(words[i].equalsIgnoreCase("your")){;
+        returnString[i] = "my";
+      }
+      else{
+        returnString[i]= words[i];
+        count += 1; /* Counts to see if none of the words match any of the key words above*/
+      }
+    }
+    /* If none of the key words above were mentioned returns a random response*/
+    if(count == words.length){
+      String[] randomResponse = {
+        "Uh-huh",
+        "Cool",
+        "Amazing",
+        "Interesting",
+        "Tell me more"
+       };
+      Random random = new Random();
+      int randomNumber = random.nextInt(5);
+      theResponse = randomResponse[randomNumber];
+      return theResponse;
+  }
+  /* If no random response is needed it joins the words back together and adds a ? */
+    else {String result = String.join(" ", returnString);
+    result = result + "?";
+      return result;
+  }
   }
 
   public static void main(String[] arguments) {
@@ -39,6 +115,5 @@ class Conversation implements Chatbot {
     Conversation myConversation = new Conversation();
     myConversation.chat();
     myConversation.printTranscript();
-
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I talked with Kylie during office hours. 
 - Which session(s) of TA / office hours did you attend?
+Friday 11-12.
 - What are your initial impressions of Java? 
-- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+I liked that you can call things from inside different functions but I keep forgetting that everything needs brackets or paranthesis around it. I am learning but it is slow and I spent a lot of time on this assignment. I'm not very clear on organization of things and where they should go espically since I'm no longer confinded by the linear thinking. But office hours helped with that and now I feel more confident. 
+- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages?
+Programming in general has the same ideas: lists, loops, functions. How they are used and the nity gritty of it is different! 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I really struggled with creating a transcript mainly because my code was disorganized because I didn't know where to organize it. I did manage to get the outputs working but be aware of capitilization. I would say follow the instructions in the video and don't try to skip ahead. 
